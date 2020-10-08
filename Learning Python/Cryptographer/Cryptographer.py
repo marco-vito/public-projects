effada7b2f8428081d1149108381b531443eb54b
Marco Vito Oddo <marcovito@id.uff.br>
2020-10-08 11:21:23 +0200

    Refactored the Substitution functions to increase readability

--- Learning Python/Cryptographer/Cryptographer.py
+++ Learning Python/Cryptographer/Cryptographer.py
@@ -21,33 +21,45 @@ def ListToString(output_list):
     return new_string
 
 
-# The next function get a list made by the input text and change each letter by a given number of steps; this function
-# allows both the encoding and the decoding of simple letter substitution messages; the function ignore special
-# characters and blank space.
+# The next functions get a list made by the input text and change each letter by a given number of steps; these
+# functions allows both the encoding and the decoding of simple letter substitution messages, depending of a boolean
+# introduced as a parameter; the function ignore special characters and blank space. The Encoding and Decoding steps
+# of the process were put in separated functions to increase readability
 
 def Substitution(input_list, steps, is_encoding=True):
     for i in range(len(input_list)):
         num = ord(input_list[i])
-        if num not in range(65, 91) and num not in range(97, 123):
+        if not input_list[i].isalpha():
             continue
         else:
-            for n in range(steps):
-                if is_encoding:
-                    num += 1
-                else:
-                    num -= 1
+            if is_encoding:
+                num = EncodingSubstitution(num, steps)
+            else:
+                num = DecodingSubstitution(num, steps)
 
-                if num == 91:
-                    num = 65
-                elif num == 123:
-                    num = 97
-                elif num == 64:
-                    num = 90
-                elif num == 96:
-                    num = 122
         input_list[i] = chr(num)
 
     return input_list
+
+
+def EncodingSubstitution(number, steps):
+    if number in range(65, 91):
+        number += steps
+        number = number - (91 - 65) if number >= 91 else number
+    elif number in range(97, 123):
+        number += steps
+        number = number - (123 - 97) if number >= 123 else number
+    return number
+
+
+def DecodingSubstitution(number, steps):
+    if number in range(65, 91):
+        number -= steps
+        number = number + (91 - 65) if number <= 64 else number
+    elif number in range(97, 123):
+        number -= steps
+        number = number + (123 - 97) if number <= 96 else number
+    return number
 
 
 # This function will encode a message by using Transposition, based on a key; that means that the characters of a
@@ -55,8 +67,9 @@ def Substitution(input_list, steps, is_encoding=True):
 # rewritten by putting together the same character column by column (instead of line by line). For this to work,
 # we need to transform our list into an array, and then reshape it; but, since arrays cannot be extended,
 # first we have to make sure the list to be transformed fits the matrix shape we'll use; to that end, we append extra
-# whitespace characters to the list that doesn't fit an exact matrix. The boolean parameter defines if the function is
-# encoding OR decoding a given message
+# whitespace characters to the list that doesn't fit an exact matrix. The boolean parameter defines if the function
+# is encoding or decoding a given message. The Encoding and Decoding steps of the process were put in separated
+# functions to increase readability
 
 def Transposition(input_list, key, is_encoding=True):
     list_height = math.ceil(len(input_list) / key)
